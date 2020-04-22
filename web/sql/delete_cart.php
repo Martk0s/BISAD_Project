a6e90eaa4708c96ecdd3c8fc307997c29c2f11a9
XSNIPERSPY <GioBlacker@gmail.com>
2020-04-22 21:59:02 +0700

    Order_list update
    - Order_list
    MISSING - DELETE
    MISSING - CHANGE PENDING TO CONFIRMED
    - delete_cart couldn't use

---
+++ web/sql/delete_cart.php
@@ -0,0 +1,26 @@
+<?php
+
+include("php/connect.php"); // Using database connection file here
+
+if(isset($_GET['order_number'])){
+    $order_number = $_GET['order_number'];
+    echo $order_number;
+  } else {
+    echo "failed";
+  }
+
+// $query = "DELETE FROM `cus_order_product` WHERE `cus_order_product`.`order_number` ='". $order_number . "'";
+
+// $del = mysqli_query($conn, $query); // delete query
+
+// if($del)
+// {
+//     mysqli_close($conn); // Close connection
+//     //header("location:order_list2.php"); // redirects to all records page
+//     exit;	
+// }
+// else
+// {
+//     echo "Error deleting record"; // display error message if not delete
+// }
+?>
--- web/sql/order_list.php
+++ web/sql/order_list.php
@@ -90,106 +90,69 @@
 <div id="shopping-cart">
 <?php
     // cus-order-Prod [Prod_id]-> $XX -> product[Prod_id]
-    $query = "SELECT * FROM `product`";
+    $query = "SELECT * FROM `cus_order` WHERE `account_id`=1 AND `order_status`='pending' ";
+    //var_dump($query);
     $result = mysqli_query($conn, $query) or die(mysqli_error());
     while($row = mysqli_fetch_array($result)) {
 ?>
 
-<table class="tbl-cart"  border ="1" cellpadding="10" cellspacing="1">
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
 <tbody>
 <tr>
-<th style="text-align:left;" >Product Name</th>
-<th style="text-align:left;" width="6%">Order ID</th>
+<th style="text-align:center;" width="7%">Order ID</th>
+<th style="text-align:left;">Product Name</th>
+<th style="text-align:center;" width="7%">Product ID</th>
 <th style="text-align:right;" width="7%">Price</th>
-<th style="text-align:right;" width="15%">Order Time</th>
+<th style="text-align:center;" width="15%">Order Time</th>
 <th style="text-align:center;" width="6%">Remove</th>
 </tr>
 <?php		
-    foreach ($result as $item){
-		?>  
-				<tr>
-				<!-- Picture -->
-				<td><img src="<?php echo $item["product_image"]; ?>" class="cart-item-image" /><?php echo $item["product_name"]; ?></td>
-				<td><?php echo $item["product_id"]; ?></td>
-				<!-- Price -->
-				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-				
-                <?php
-                    $query = "SELECT * FROM `cus_order_product`";
+    foreach ($result as $order){
+        ?>
+        <?php
+        $order_id = $order['order_id'];
+        $query = "SELECT * FROM `cus_order_product` WHERE `order_id`=" . $order_id;
+        $result = mysqli_query($conn, $query) or die(mysqli_error());
+        while($row = mysqli_fetch_array($result)) {
+            foreach ($result as $prod){
+		?>      
+				<tr>			
+                <?php                
+                    $prod_id = $prod['product_id'];
+                    $query = "SELECT * FROM `product` WHERE `product_id` =". $prod_id;
                     $result = mysqli_query($conn, $query) or die(mysqli_error());
                     while($row = mysqli_fetch_array($result)) {
-                        foreach ($result as $item){
-                            echo "<td  style='text-align:right;'> <?php echo" . $item['order_number'].";?> </td>";
+                        foreach ($result as $item){ ?>
+                            <!-- Order ID -->
+                            <td style="text-align:center;"><?php echo $order["order_id"]; ?></td>
+                            <!-- Picture -->
+				            <td><img src="<?php echo $item["product_image"]; ?>" class="cart-item-image" /><?php echo $item["product_name"]; ?></td>
+				            <td style="text-align:center;"><?php echo $item["product_id"]; ?></td>
+				            <!-- Price -->
+				            <td  style="text-align:right;"><?php echo "$" . $item["price"]; ?></td>
+                            <td style="text-align:center;"><?php echo $prod["order_number"]; ?></td>
+                        <?php
                         }
                     }
                 ?>
 				<!-- Remove btn action remove&product code -->
-				<td style="text-align:center;"><a href="" class="btnRemoveAction"><img src="img/icon-delete.png"  alt="Remove Item" /></a></td>
-				</tr>
+				<td style="text-align:center;"><a href="delete_cart.php?id=<?php echo $prod['order_number'];?>"><img src="img/icon-delete.png"  alt="Remove Item" /></a></td>
+                <?php 
+                // echo "<br>";
+                // var_dump($prod['order_number']); 
+                ?> 
+                </tr>
 				<?php
-		}
-		?>
+            }
+        }
+    }
+		        ?>
 </tbody>
 </table>
 <?php
 }
 ?>
 </div>
-    <!--?php
-    // include("php/connect.php");
-    $query = "SELECT * FROM `product`";
-    $result = mysqli_query($conn, $query) or die(mysqli_error());
-    while($row = mysqli_fetch_array($result)) {
-        echo "<div class='container py-3'>";
-        echo "<div class='card'>";
-        echo "<div class='row'>";
-        echo "<div class='col-md-4'>";
-        echo "<img src='" . $row['product_image'] . "' class='shoes-image'>";
-        
-        echo "</div>";
-        echo "<div class='col-md-8 px-3 py-3'>";
-        echo "<div class='card-block px-3'>";
-        $brand_id = $row["brand_id"];
-        $query = "SELECT * FROM `brand` WHERE brand_id='$brand_id'";
-        $brand_id = mysqli_query($conn, $query) or die(mysqli_error());
-        while($brand_name = mysqli_fetch_array($brand_id)) {
-            echo "<h2 class='card-title'>" . $brand_name["brand_name"] . "</h2>";//brand name
-        }
-        echo "<p class='card-text'> <b>Model</b><br>&ensp;&ensp;" .$row["product_name"] . "</p>";//name
-        echo "<p class='card-text'> <b>Type</b><br>&ensp;&ensp;" .$row["product_type"] . "</p>";//type
-        echo "<hr>";
-        echo "<p class='card-text'> <b>Gender</b><br>&ensp;&ensp;" .$row["gender"] . "</p>";//gender
-        echo "<p class='card-text'>";
-
-        $size_list = explode(",", $row["size_uk"]);
-        echo "<b>Size (UK) &ensp;</b>";//size (UK)
-        foreach ($size_list as $UK) {
-            echo $UK . "&ensp;&ensp;";
-        }
-        echo "<br>";
-        echo "</p>";
-
-        echo "<div class='container'>";
-        echo "<div class='row'>";
-        echo "<p class='card-text'><b>Color&ensp;</b></p>";
-        echo "<div class='color-box " . $row["color"] . "'></div>";//color box
-        echo "<p class='card-text'>" . $row["color"] . "</p>";//color text
-        echo "</div>";
-        echo "</div>";
-        echo "<h4 style='color: #00a2e2;'>$ " . $row["price"] . "</h4>";//price ($)
-        echo "<div>";
-        echo '<form action="update.php" method="POST">
-        <input type="hidden" name="_id" value="' . $row["product_id"] . '">
-        <button type="input" class="btn btn-success update-btn"><i class="fas fa-cart-plus"></i> Add to cart</button>
-        </form>';
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-    }
-    ?-->
 </body>
 <script>
 // -=[Scroll To Top Button script]=- //
